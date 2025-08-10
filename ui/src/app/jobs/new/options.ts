62dedf13180f43461bacd902ff02547f7e8c9fa4
A.C.T. SOON® <86653128+AlekseyCalvin@users.noreply.github.com>
2025-08-10 03:32:58 -0700

    Update options.ts

--- ui/src/app/jobs/new/options.ts
+++ ui/src/app/jobs/new/options.ts
@@ -3,7 +3,7 @@ import { GroupedSelectOption, SelectOption } from '@/types';
 type Control = 'depth' | 'line' | 'pose' | 'inpaint';
 
 type DisableableSections = 'model.quantize' | 'train.timestep_type' | 'network.conv';
-type AdditionalSections = 'datasets.control_path' | 'sample.ctrl_img' | 'datasets.num_frames' | 'model.low_vram';
+type AdditionalSections = 'datasets.control_path' | 'datasets.do_i2v' | 'sample.ctrl_img' | 'datasets.num_frames' | 'model.low_vram';
 type ModelGroup = 'image' | 'video';
 
 export interface ModelArch {

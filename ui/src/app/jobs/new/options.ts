b1eaa628057f7575b1020c03022a5db809e210f1
A.C.T. SOON® <86653128+AlekseyCalvin@users.noreply.github.com>
2025-08-10 03:33:32 -0700

    Update options.ts

--- ui/src/app/jobs/new/options.ts
+++ ui/src/app/jobs/new/options.ts
@@ -201,8 +201,7 @@ export const modelArchs: ModelArch[] = [
       'config.process[0].train.timestep_type': ['weighted', 'sigmoid'],
     },
     disableSections: ['network.conv'],
-    additionalSections: ['sample.ctrl_img', 'datasets.num_frames', 'model.low_vram'],
-  },
+    additionalSections: ['sample.ctrl_img', 'datasets.num_frames', 'model.low_vram', 'datasets.do_i2v'],  },
   {
     name: 'lumina2',
     label: 'Lumina2',

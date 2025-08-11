006baf5c3cb2013bafcec09840f4d23f7866eb11
A.C.T. SOON® <86653128+AlekseyCalvin@users.noreply.github.com>
2025-08-11 07:38:49 -0700

    Update base_model.py

--- toolkit/models/base_model.py
+++ toolkit/models/base_model.py
@@ -227,7 +227,11 @@ class BaseModel:
     @property
     def is_flux(self):
         return self.arch == 'flux'
-
+        
+    @property
+    def is_flux_kontext(self):
+        return self.arch == 'flux_kontext'
+        
     @property
     def is_lumina2(self):
         return self.arch == 'lumina2'
